f26921e74618870ccaa30ca1c82e6f1af61809b0
= <=>
2020-07-05 12:33:15 -0300

    Add entity of Settings to Down XMLs

---
+++ src/entity/SettingsDownNotes.ts
@@ -0,0 +1,19 @@
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm'
+
+@Entity()
+export default class SettingsDownNotes {
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @CreateDateColumn()
+    createdAt: Date
+
+    @UpdateDateColumn()
+    updatedAt: Date
+
+    @Column({ default: 'C:/notas_fiscais/goiania/logs/hourLog/accessGoiania/nameCompanieWithIM' })
+    folderToSaveLog: String
+
+    @Column({ default: 'C:/notas_fiscais/goiania/xmls/nameCompanieWithIM/year/month' })
+    folderToSaveXMLs: String
+}
