52d3d5c08513030a1c7ff5b7440cd0d3d6fbc958
= <=>
2020-07-27 07:08:17 -0300

    Add messageLogToShowUser

--- src/entity/LogPrefGoiania.ts
+++ src/entity/LogPrefGoiania.ts
@@ -41,6 +41,9 @@ export default class LogPrefGoiania {
     @Column()
     messageLog: String
 
+    @Column()
+    messageLogToShowUser: String
+
     @Column({ nullable: true })
     messageError: String
 
