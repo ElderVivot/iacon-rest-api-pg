6cc7bcad85c4e2d9169ec91d262425ec7939a2db
= <=>
2020-07-30 20:38:31 -0300

    Add ormconfig to identific is js or ts. And fixed version in package.json

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -2,18 +2,23 @@ import path from 'path'
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions'
 import 'dotenv/config'
 
+let typeFile = 'ts'
+if (process.env.PRODUCTION === 'false') {
+    typeFile = 'js'
+}
+
 const typeOrmConfig: PostgresConnectionOptions = {
     type: 'postgres',
-    host: 'localhost',
+    host: process.env.DB_HOST || 'localhost',
     port: Number(process.env.PORT) || 5432,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
     synchronize: true,
     logging: false,
-    entities: [path.resolve(__dirname, '../entity/**/*.ts')],
-    migrations: [path.resolve(__dirname, '../migrations/**/*.ts')],
-    subscribers: [path.resolve(__dirname, '../subscriber/**/*.ts')],
+    entities: [path.resolve(__dirname, `../entity/**/*.${typeFile}`)],
+    migrations: [path.resolve(__dirname, `../migrations/**/*.${typeFile}`)],
+    subscribers: [path.resolve(__dirname, `../subscriber/**/*.${typeFile}`)],
     cli: {
         entitiesDir: path.resolve(__dirname, '../entity'),
         migrationsDir: path.resolve(__dirname, '../migrations'),
