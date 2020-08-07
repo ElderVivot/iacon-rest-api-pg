61e6e13f83f0ab1a59a1eb70a8980a90868ad7aa
= <=>
2020-08-07 13:28:55 -0300

    Update to LogPrefGoianiaErrors get log only less 7 times

--- src/controllers/LogPrefGoianiaErrors.ts
+++ src/controllers/LogPrefGoianiaErrors.ts
@@ -6,7 +6,16 @@ import LogPrefGoiania from '../entity/LogPrefGoiania'
 class LogPrefGoianiaErrors {
     async index (request: Request, response: Response): Promise<LogPrefGoiania[] | any> {
         try {
-            const logPrefGoiania = await getRepository(LogPrefGoiania).find({ typeLog: 'error' })
+            const logPrefGoiania = await getRepository(LogPrefGoiania).query(
+                `SELECT logs."id", logs."inscricaoMunicipal", logs."dateStartDown", logs."dateEndDown",
+                        logs."qtdTimesReprocessed", access."user", access."password", access."id" AS idUser
+                   FROM log_pref_goiania AS logs
+                        INNER JOIN pref_goiania_access AS access
+                             ON    access."id" = logs."prefGoianiaAccessId"
+                  WHERE logs."typeLog" = 'error'
+                    AND logs."qtdTimesReprocessed" <= 7`,
+                []
+            )
             console.log(`- [controllers-LogPrefGoianiaErrors.index] --> Success --> ${logPrefGoiania.length} length`)
             return response.json(logPrefGoiania)
         } catch (error) {
