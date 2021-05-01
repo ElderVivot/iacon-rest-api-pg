d04460b5bc92fca81f8c9dd932b1b27973723d38
ElderVivot <eldervivot@gmail.com>
2021-05-01 07:15:26 -0300

    feat: add log to per month

--- src/controllers/LogNfeNfceGOMaxDateEndDown.ts
+++ src/controllers/LogNfeNfceGOMaxDateEndDown.ts
@@ -6,16 +6,17 @@ import LogNfeNfceGO from '../entity/LogNfeNfceGO'
 class LogNfeNfceGOMaxDateEndDown {
     async show (request: Request, response: Response): Promise<LogNfeNfceGO | any> {
         try {
-            const { cgceCompanie, modelNF } = request.query
+            const { cgceCompanie, modelNF, month, year } = request.query
             const logNfeNfceGO = await getRepository(LogNfeNfceGO).query(
                 `SELECT MAX(logs."dateEndDown") AS dateDownMax
                    FROM log_nfe_nfce_go AS logs
-                  WHERE logs."cgceCompanie" = $1
-                    AND logs."modelNF" = $2
+                  WHERE logs."cgceCompanie" = '${cgceCompanie}'
+                    AND logs."modelNF" = '${modelNF}'
                     AND ( logs."typeLog" IN ('success')
                         OR ( logs."typeLog" IN ('warning') 
-                           AND logs."messageError" IN ('NOT_EXIST_NOTES_TO_DOWN', 'NOT_EXIST_NOTES') ) )`,
-                [cgceCompanie, modelNF]
+                           AND logs."messageError" IN ('NOT_EXIST_NOTES_TO_DOWN', 'NOT_EXIST_NOTES') ) )
+                    AND EXTRACT(MONTH FROM logs."dateStartDown") = ${month}
+                    AND EXTRACT(YEAR FROM logs."dateStartDown") = ${year}`
             )
             console.log(`- [controllers-LogNfeNfceGOMaxDateEndDown.show] --> Success --> ${logNfeNfceGO.length} length`)
             return response.json(logNfeNfceGO[0])
